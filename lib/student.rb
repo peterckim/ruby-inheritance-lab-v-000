de96dbf1a1eb06d6a892fbd9e02827bbf1ea7e80
Peter Kim <peterckim96@gmail.com>
2018-06-09 22:40:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,14 @@ class Student < User
   @attr_accessor :first_name, :last_name
   
   def initialize
-    KNOWLEDGE = []
+    @knowledge = []
+  end
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  
+  def knowledge
+    @knowledge
   end
 end
