3e0532fdbc147931b1adecab39871c0749c4bf27
Peter Kim <peterckim96@gmail.com>
2018-06-09 22:35:11 +0000

    automatically backed up by learn

---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User
+    def initialize(first_name, last_name)
+      @first_name = first_name
+      @last_name = last_name
+    end
+    
+    def first_name
+      @first_name
+    end
+    
+    def last_name
+      @last_name
+    end
+end
